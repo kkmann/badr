dee5fa9eb4a439e279b1cdf89dc23c87b61a7b49
Kevin Kunzmann <kevin.kunzmann@mrc-bsu.cam.ac.uk>
2019-11-20 11:04:02 +0000

    fix onload

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
     tryCatch({
         JuliaCall::julia_command('using bad')
     }, error = function(e) {
-        cat(e)
+        print(e)
         stop("Could not load julia package 'bad', make sure julia is found and 'bad' is installed. Try running 'badr::setup_julia()' with optional arguments for 'JuliaCall::julia_setup()' to install.")
     })
 }
