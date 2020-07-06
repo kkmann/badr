82f664df60fa4efae5077ed9c5f10902e5457de1
Kevin Kunzmann <kevinkunzmann@gmx.net>
2020-07-06 11:38:56 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -13,7 +13,6 @@ load_julia_package <- function(libname, pkgname){
     tryCatch({
         JuliaCall::julia_setup(verbose = FALSE, install = FALSE, useRCall = FALSE)
         JuliaCall::julia_command('using bad')
-        packageStartupMessage("Loaded julia package 'bad' successfully.")
     }, error = function(e) {
         packageStartupMessage("Could not load julia package 'bad', make sure julia is found and 'bad' is installed. Try running 'badr::setup_julia_package()' with optional arguments for 'JuliaCall::julia_setup()' to install.")
     })
